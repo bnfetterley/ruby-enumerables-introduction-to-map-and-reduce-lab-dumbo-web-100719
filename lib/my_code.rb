95ac36ecfaf383157ddcbefd29cf8081e4748876
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-28 01:18:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,6 +62,8 @@ def reduce_to_any_true(source_array)
   i = 0 
   while i < source_array.size do 
      return true if source_array[i] == [i]
+   else
+     return false if source_array[i] == ![i]
      i += 1 
    end
    return true
