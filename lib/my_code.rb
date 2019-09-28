c22c5192e1f015ee906ca02fefc87d91e860b8d5
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-28 01:09:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,8 +51,9 @@ end
 
 def reduce_to_all_true(source_array)
   i = 0 
+  false = nil
   while i < source_array.size do 
-     return false if source_array[i] == "string"
+     return false if source_array[i] == nil
      i += 1 
    end
    return true
